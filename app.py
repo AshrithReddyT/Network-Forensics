8321f6da2e4f7c1b5cd4ba160fa2e140415c7843
AshrithReddyT <ashrithreddyt@gmail.com>
2019-06-01 10:07:29 +0530

    Added viewnode and Incidents

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, render_template
 import glob
 import os
+import json
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -8,9 +9,14 @@ app = Flask(__name__, static_url_path='/static')
 def index():
     return render_template('network.html')
 
-@app.route('/events')
-def events():
-    list_of_files = glob.glob('./logs/*.log') # * means all if need specific format then *.csv
+@app.route('/viewnode', methods = ['GET'])
+def viewnode():
+    node = request.args.get('node')
+    return render_template('viewnode.html', node=node)
+
+@app.route('/incidents')
+def incidents():
+    list_of_files = glob.glob('./logs/*.log') 
     latest_file = max(list_of_files, key=os.path.getctime)
     lines = []
     with open(latest_file,"r") as f:
@@ -18,7 +24,7 @@ def events():
         while line:
             lines.append(line.split(","))
             line = f.readline()
-    return render_template('events.html', data=lines)
+    return render_template('incidents.html', data=lines)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8000', debug=True)
